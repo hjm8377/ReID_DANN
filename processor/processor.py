8877187218a7882c08ded0a996bd2682d60278d1
hjm8377 <hjm8377@gmail.com>
2025-09-02 13:28:33 +0900

    modify train code

--- processor/processor.py
+++ processor/processor.py
@@ -3,6 +3,7 @@ import os
 import time
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from utils.meter import AverageMeter
 from utils.metrics import R1_mAP_eval
 from torch import amp
@@ -61,8 +62,10 @@ def do_train(cfg,
         source_iter = iter(source_loader)
         target_iter = iter(target_loader)
 
+        iters_per_epoch = max(len(source_loader), len(target_loader))
+
         # for n_iter, (img, vid, target_cam, target_view) in enumerate(train_loader):
-        for n_iter in range(max(len(source_loader), len(target_loader))):
+        for n_iter in range(iters_per_epoch):
             optimizer.zero_grad()
             optimizer_center.zero_grad()
 
@@ -72,27 +75,50 @@ def do_train(cfg,
             # target_view = target_view.to(device)
 
             try:
-                s_img, s_vid, s_target_cam, s_target_view, _ = next(source_iter)
+                s_img, s_vid, s_cam, s_view = next(source_iter)
             except:
                 source_iter = iter(source_loader)
-                s_img, s_vid, s_target_cam, s_target_view, _ = next(source_iter)
+                s_img, s_vid, s_cam, s_view = next(source_iter)
 
             try:
-                t_img, _, t_target_cam, t_target_view, _ = next(target_iter)
+                t_img, t_vid_dummy, t_cam, t_view = next(target_iter)
             except:
                 target_iter = iter(target_loader)
-                t_img, _, t_target_cam, t_target_view, _ = next(target_iter)
+                t_img, t_vid_dummy, t_cam, t_view = next(target_iter)
 
-            s_img, s_vid, s_cam            
+            s_img, s_vid, s_cam, s_view = s_img.to(device), s_vid.to(device), s_cam.to(device), s_view.to(device)
+            t_img, t_cam, t_view = t_img.to(device), t_cam.to(device), t_view.to(device)
             
-            
-            lambda_d = dann_lambda()
-
-            model.set_lambda_d(lambda_d)
+            lambda_d = dann_lambda(global_step, total_steps)
+            if hasattr(model, "set_lambda_d"):
+                model.set_lambda_d(lambda_d)
+            elif hasattr(model, "module") and hasattr(model.module, "set_lambda_d"):
+                model.module.set_lambda_d(lambda_d)
 
             with amp.autocast(enabled=True):
-                score, feat = model(img, target, cam_label=target_cam, view_label=target_view )
-                loss = loss_fn(score, feat, target, target_cam)
+                out_s = model(s_img, s_vid, cam_label=s_cam, view_label=s_view, domain_only=False)
+                score_s, feat_s, dom_s = out_s
+
+                out_t = model(t_img, cam_label=t_cam, view_label=t_view, domain_only=True)
+                _, feat_t, dom_t = out_t
+
+
+                loss_id_tri = loss_fn(score_s,feat_s, s_vid, s_cam)
+
+                # domain loss
+                dom_label_s = torch.zeros(dom_s.shape[0]).long().to(device)
+                dom_label_t = torch.ones(dom_t.shape[0]).long().to(device)
+                
+                loss_dom_s = F.cross_entropy(dom_s, dom_label_s)
+                loss_dom_t = F.cross_entropy(dom_t, dom_label_t)
+
+                Ns, Nt = dom_s.size(0), dom_t.size(0)
+                loss_dom = (loss_dom_s * Ns + loss_dom_t * Nt) / (Ns + Nt)
+
+                loss = loss_id_tri + lambda_d * loss_dom
+
+                # score, feat = model(img, target, cam_label=target_cam, view_label=target_view )
+                # loss = loss_fn(score, feat, target, target_cam)
 
             scaler.scale(loss).backward()
 
@@ -104,13 +130,22 @@ def do_train(cfg,
                     param.grad.data *= (1. / cfg.SOLVER.CENTER_LOSS_WEIGHT)
                 scaler.step(optimizer_center)
                 scaler.update()
-            if isinstance(score, list):
-                acc = (score[0].max(1)[1] == target).float().mean()
-            else:
-                acc = (score.max(1)[1] == target).float().mean()
+            # if isinstance(score, list):
+            #     acc = (score[0].max(1)[1] == target).float().mean()
+            # else:
+            #     acc = (score.max(1)[1] == target).float().mean()
 
-            loss_meter.update(loss.item(), img.shape[0])
+            # loss_meter.update(loss.item(), img.shape[0])
+            # acc_meter.update(acc, 1)
+
+            acc = (score_s.max(1)[1] == s_vid).float().mean()
+
+            # 손실은 전체 배치 크기(소스+타겟) 기준으로 기록하는 것이 좋음
+            total_batch_size = s_img.shape[0] + t_img.shape[0]
+            loss_meter.update(loss.item(), total_batch_size)
             acc_meter.update(acc, 1)
+
+            global_step += 1
 
             torch.cuda.synchronize()
             if cfg.MODEL.DIST_TRAIN:
@@ -118,12 +153,12 @@ def do_train(cfg,
                     if (n_iter + 1) % log_period == 0:
                         base_lr = scheduler._get_lr(epoch)[0] if cfg.SOLVER.WARMUP_METHOD == 'cosine' else scheduler.get_lr()[0]
                         logger.info("Epoch[{}] Iter[{}/{}] Loss: {:.3f}, Acc: {:.3f}, Base Lr: {:.2e}"
-                                    .format(epoch, (n_iter + 1), len(train_loader), loss_meter.avg, acc_meter.avg, base_lr))
+                                    .format(epoch, (n_iter + 1), iters_per_epoch, loss_meter.avg, acc_meter.avg, base_lr))
             else:
                 if (n_iter + 1) % log_period == 0:
                     base_lr = scheduler._get_lr(epoch)[0] if cfg.SOLVER.WARMUP_METHOD == 'cosine' else scheduler.get_lr()[0]
                     logger.info("Epoch[{}] Iter[{}/{}] Loss: {:.3f}, Acc: {:.3f}, Base Lr: {:.2e}"
-                                .format(epoch, (n_iter + 1), len(train_loader), loss_meter.avg, acc_meter.avg, base_lr))
+                                .format(epoch, (n_iter + 1), iters_per_epoch, loss_meter.avg, acc_meter.avg, base_lr))
 
         end_time = time.time()
         time_per_batch = (end_time - start_time) / (n_iter + 1)
@@ -134,8 +169,10 @@ def do_train(cfg,
         if cfg.MODEL.DIST_TRAIN:
             pass
         else:
+            total_bs = cfg.SOLVER.IMS_PER_BATCH
+            speed = total_bs / time_per_batch
             logger.info("Epoch {} done. Time per epoch: {:.3f}[s] Speed: {:.1f}[samples/s]"
-                    .format(epoch, time_per_batch * (n_iter + 1), train_loader.batch_size / time_per_batch))
+                    .format(epoch, time_per_batch * (n_iter + 1), speed))
 
         if epoch % checkpoint_period == 0:
             if cfg.MODEL.DIST_TRAIN:
